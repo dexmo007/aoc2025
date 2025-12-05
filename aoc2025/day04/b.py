562ba8173ae2e34374b12afb3fccd5553db731a8
dexmo007 <henrik.drefs@outlook.de>
2025-12-05 16:01:29 +0100

    day 4

---
+++ aoc2025/day04/b.py
@@ -0,0 +1,31 @@
+from ..core import open_file
+
+
+with open_file() as f:
+    grid = [[1 if c == "@" else 0 for c in line.strip()] for line in f]
+
+
+removed = True
+total_removed = 0
+while removed:
+    removed = False
+
+    for i, row in enumerate(grid):
+        for j, val in enumerate(row):
+            if not val:
+                continue
+            adjacent = 0
+            if i > 0:
+                adjacent += sum(grid[i - 1][max(j - 1, 0) : min(j + 2, len(row))])
+            if j > 0:
+                adjacent += row[j - 1]
+            if j < len(row) - 1:
+                adjacent += row[j + 1]
+            if i < len(grid) - 1:
+                adjacent += sum(grid[i + 1][max(j - 1, 0) : min(j + 2, len(row))])
+            if adjacent < 4:
+                row[j] = False
+                removed = True
+                total_removed += 1
+
+print(total_removed)
